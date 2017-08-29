99112f12228343656c8c3072714037617f9ccbe5
Maik Messerschmidt <maik.messerschmidt@gmx.net>
2017-08-29 10:58:37 +0200

    Added hardware keyboard support and 'DEL' button.

--- sudokuapp.py
+++ sudokuapp.py
@@ -8,8 +8,11 @@ from os.path import join
 # kivy imports
 from kivy.app import App
 from kivy.config import Config
-from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+
 
 # needed by sudoku.kv
 from sudokulib.grid import SudokuGrid
@@ -20,6 +23,37 @@ class SudokuWidget(BoxLayout):
     grid = ObjectProperty(None)
     menu = ObjectProperty(None)
     info_label = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(SudokuWidget, self).__init__(**kwargs)
+
+        # Add hardware keyboard support
+        self.__keyboard = Window.request_keyboard(
+            self.__on_keyboard_closed, self, 'text')
+
+        if self.__keyboard.widget:
+            # This is a virtual keyboard - throw it away
+            self.__keyboard.release()
+        else:
+            self.__keyboard.bind(on_key_down=self.__on_keyboard_down)
+
+        self.KEYS = {}
+        for i in range(10):
+            self.KEYS["%d" % i] = i
+            self.KEYS["numpad%d" % i] = i
+
+        self.KEYS["backspace"] = 0
+        self.KEYS["delete"] = 0
+
+    def __on_keyboard_closed(self):
+        self.__keyboard.unbind(on_key_down=self.__on_keyboard_down)
+        self.__keyboard = None
+
+    def __on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        keyname = keycode[1]
+        input = self.KEYS.get(keyname, None)
+        if input is not None:
+            self.grid.enter_number(input)
 
     def save_state(self, filename=STATEFILE):
         self.grid.save_state(filename=filename)
